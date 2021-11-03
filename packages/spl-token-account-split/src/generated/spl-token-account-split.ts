7471c6c0c416b2c4d37298858e89690630fe9322
Chewing Glass <chewinglass123@gmail.com>
2021-11-03 16:36:04 -0500

    Remove version from docs

--- packages/spl-token-account-split/src/generated/spl-token-account-split.ts
+++ packages/spl-token-account-split/src/generated/spl-token-account-split.ts
@@ -1,201 +1,281 @@
-import { IdlAccounts, Idl } from '@project-serum/anchor';
-export const SplTokenAccountSplitIDLJson: Idl & { metadata?: { address: string } } = {
-  "version": "0.0.0",
-  "name": "spl_token_account_split",
-  "instructions": [
+import { IdlAccounts, Idl } from "@project-serum/anchor";
+export const SplTokenAccountSplitIDLJson: Idl & {
+  metadata?: { address: string };
+} = {
+  version: "0.0.0",
+  name: "spl_token_account_split",
+  instructions: [
     {
-      "name": "initializeTokenAccountSplitV0",
-      "accounts": [
+      name: "initializeTokenAccountSplitV0",
+      accounts: [
         {
-          "name": "payer",
-          "isMut": true,
-          "isSigner": true
+          name: "payer",
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "tokenAccountSplit",
-          "isMut": true,
-          "isSigner": false
+          name: "tokenAccountSplit",
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "tokenAccount",
-          "isMut": false,
-          "isSigner": false
+          name: "tokenAccount",
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "tokenStaking",
-          "isMut": false,
-          "isSigner": false
+          name: "tokenStaking",
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        }
+          name: "rent",
+          isMut: false,
+          isSigner: false,
+        },
       ],
-      "args": [
+      args: [
         {
-          "name": "args",
-          "type": {
-            "defined": "InitializeTokenAccountSplitV0Args"
-          }
-        }
-      ]
-    },
-    {
-      "name": "collectTokensV0",
-      "accounts": [
-        {
-          "name": "tokenAccountSplit",
-          "isMut": false,
-          "isSigner": false
+          name: "args",
+          type: {
+            defined: "InitializeTokenAccountSplitV0Args",
+          },
         },
-        {
-          "name": "tokenAccount",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "tokenAccountAuthority",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "tokenStaking",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "targetMint",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "stakingRewardsSource",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "stakingRewardsAuthority",
-          "isMut": false,
-          "isSigner": true
-        },
-        {
-          "name": "destination",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        }
       ],
-      "args": [
+    },
+    {
+      name: "collectTokensV0",
+      accounts: [
         {
-          "name": "args",
-          "type": {
-            "defined": "CollectTokensV0Args"
-          }
-        }
-      ]
-    }
+          name: "tokenAccountSplit",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "tokenAccount",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "tokenAccountAuthority",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "tokenStaking",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "targetMint",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "stakingRewardsSource",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "stakingRewardsAuthority",
+          isMut: false,
+          isSigner: true,
+        },
+        {
+          name: "destination",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "tokenProgram",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "clock",
+          isMut: false,
+          isSigner: false,
+        },
+      ],
+      args: [
+        {
+          name: "args",
+          type: {
+            defined: "CollectTokensV0Args",
+          },
+        },
+      ],
+    },
   ],
-  "accounts": [
+  accounts: [
     {
-      "name": "TokenAccountSplitV0",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "TokenAccountSplitV0",
+      type: {
+        kind: "struct",
+        fields: [
           {
-            "name": "tokenAccount",
-            "type": "publicKey"
+            name: "tokenAccount",
+            type: "publicKey",
           },
           {
-            "name": "tokenStaking",
-            "type": "publicKey"
+            name: "tokenStaking",
+            type: "publicKey",
           },
           {
-            "name": "slotNumber",
-            "type": "u16"
+            name: "slotNumber",
+            type: "u16",
           },
           {
-            "name": "bumpSeed",
-            "type": "u8"
+            name: "bumpSeed",
+            type: "u8",
           },
           {
-            "name": "tokenAccountAuthorityBumpSeed",
-            "type": "u8"
-          }
-        ]
-      }
-    }
+            name: "tokenAccountAuthorityBumpSeed",
+            type: "u8",
+          },
+        ],
+      },
+    },
   ],
-  "types": [
+  types: [
     {
-      "name": "InitializeTokenAccountSplitV0Args",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "InitializeTokenAccountSplitV0Args",
+      type: {
+        kind: "struct",
+        fields: [
           {
-            "name": "slotNumber",
-            "type": "u16"
+            name: "slotNumber",
+            type: "u16",
           },
           {
-            "name": "bumpSeed",
-            "type": "u8"
+            name: "bumpSeed",
+            type: "u8",
           },
           {
-            "name": "tokenAccountAuthorityBumpSeed",
-            "type": "u8"
-          }
-        ]
-      }
+            name: "tokenAccountAuthorityBumpSeed",
+            type: "u8",
+          },
+        ],
+      },
     },
     {
-      "name": "CollectTokensV0Args",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "CollectTokensV0Args",
+      type: {
+        kind: "struct",
+        fields: [
           {
-            "name": "stakingRewardsAmount",
-            "type": "u64"
-          }
-        ]
-      }
-    }
+            name: "stakingRewardsAmount",
+            type: "u64",
+          },
+        ],
+      },
+    },
   ],
-  "errors": [
+  errors: [
     {
-      "code": 300,
-      "name": "InvalidTokenAccountAuthority",
-      "msg": "Invalid token accountn authority"
+      code: 300,
+      name: "InvalidTokenAccountAuthority",
+      msg: "Invalid token accountn authority",
     },
     {
-      "code": 301,
-      "name": "ArithmeticError",
-      "msg": "Overflow or other checked arithmetic error"
-    }
+      code: 301,
+      name: "ArithmeticError",
+      msg: "Overflow or other checked arithmetic error",
+    },
   ],
-  "metadata": {
-    "address": "Sp1it1Djn2NmQvXLPnGM4zAXArxuchvSdytNt5n76Hm"
-  }
+  metadata: {
+    address: "Sp1it1Djn2NmQvXLPnGM4zAXArxuchvSdytNt5n76Hm",
+  },
 };
-export type SplTokenAccountSplitIDL = {"version":"0.0.0","name":"spl_token_account_split","instructions":[{"name":"initializeTokenAccountSplitV0","accounts":[{"name":"payer","isMut":true,"isSigner":true},{"name":"tokenAccountSplit","isMut":true,"isSigner":false},{"name":"tokenAccount","isMut":false,"isSigner":false},{"name":"tokenStaking","isMut":false,"isSigner":false},{"name":"systemProgram","isMut":false,"isSigner":false},{"name":"rent","isMut":false,"isSigner":false}],"args":[{"name":"args","type":{"defined":"InitializeTokenAccountSplitV0Args"}}]},{"name":"collectTokensV0","accounts":[{"name":"tokenAccountSplit","isMut":false,"isSigner":false},{"name":"tokenAccount","isMut":true,"isSigner":false},{"name":"tokenAccountAuthority","isMut":false,"isSigner":false},{"name":"tokenStaking","isMut":false,"isSigner":false},{"name":"targetMint","isMut":true,"isSigner":false},{"name":"stakingRewardsSource","isMut":true,"isSigner":false},{"name":"stakingRewardsAuthority","isMut":false,"isSigner":true},{"name":"destination","isMut":true,"isSigner":false},{"name":"tokenProgram","isMut":false,"isSigner":false},{"name":"clock","isMut":false,"isSigner":false}],"args":[{"name":"args","type":{"defined":"CollectTokensV0Args"}}]}],"accounts":[{"name":"tokenAccountSplitV0","type":{"kind":"struct","fields":[{"name":"tokenAccount","type":"publicKey"},{"name":"tokenStaking","type":"publicKey"},{"name":"slotNumber","type":"u16"},{"name":"bumpSeed","type":"u8"},{"name":"tokenAccountAuthorityBumpSeed","type":"u8"}]}}],"types":[{"name":"InitializeTokenAccountSplitV0Args","type":{"kind":"struct","fields":[{"name":"slotNumber","type":"u16"},{"name":"bumpSeed","type":"u8"},{"name":"tokenAccountAuthorityBumpSeed","type":"u8"}]}},{"name":"CollectTokensV0Args","type":{"kind":"struct","fields":[{"name":"stakingRewardsAmount","type":"u64"}]}}],"errors":[{"code":300,"name":"InvalidTokenAccountAuthority","msg":"Invalid token accountn authority"},{"code":301,"name":"ArithmeticError","msg":"Overflow or other checked arithmetic error"}],"metadata":{"address":"Sp1it1Djn2NmQvXLPnGM4zAXArxuchvSdytNt5n76Hm"}};
+export type SplTokenAccountSplitIDL = {
+  version: "0.0.0";
+  name: "spl_token_account_split";
+  instructions: [
+    {
+      name: "initializeTokenAccountSplitV0";
+      accounts: [
+        { name: "payer"; isMut: true; isSigner: true },
+        { name: "tokenAccountSplit"; isMut: true; isSigner: false },
+        { name: "tokenAccount"; isMut: false; isSigner: false },
+        { name: "tokenStaking"; isMut: false; isSigner: false },
+        { name: "systemProgram"; isMut: false; isSigner: false },
+        { name: "rent"; isMut: false; isSigner: false }
+      ];
+      args: [
+        { name: "args"; type: { defined: "InitializeTokenAccountSplitV0Args" } }
+      ];
+    },
+    {
+      name: "collectTokensV0";
+      accounts: [
+        { name: "tokenAccountSplit"; isMut: false; isSigner: false },
+        { name: "tokenAccount"; isMut: true; isSigner: false },
+        { name: "tokenAccountAuthority"; isMut: false; isSigner: false },
+        { name: "tokenStaking"; isMut: false; isSigner: false },
+        { name: "targetMint"; isMut: true; isSigner: false },
+        { name: "stakingRewardsSource"; isMut: true; isSigner: false },
+        { name: "stakingRewardsAuthority"; isMut: false; isSigner: true },
+        { name: "destination"; isMut: true; isSigner: false },
+        { name: "tokenProgram"; isMut: false; isSigner: false },
+        { name: "clock"; isMut: false; isSigner: false }
+      ];
+      args: [{ name: "args"; type: { defined: "CollectTokensV0Args" } }];
+    }
+  ];
+  accounts: [
+    {
+      name: "tokenAccountSplitV0";
+      type: {
+        kind: "struct";
+        fields: [
+          { name: "tokenAccount"; type: "publicKey" },
+          { name: "tokenStaking"; type: "publicKey" },
+          { name: "slotNumber"; type: "u16" },
+          { name: "bumpSeed"; type: "u8" },
+          { name: "tokenAccountAuthorityBumpSeed"; type: "u8" }
+        ];
+      };
+    }
+  ];
+  types: [
+    {
+      name: "InitializeTokenAccountSplitV0Args";
+      type: {
+        kind: "struct";
+        fields: [
+          { name: "slotNumber"; type: "u16" },
+          { name: "bumpSeed"; type: "u8" },
+          { name: "tokenAccountAuthorityBumpSeed"; type: "u8" }
+        ];
+      };
+    },
+    {
+      name: "CollectTokensV0Args";
+      type: {
+        kind: "struct";
+        fields: [{ name: "stakingRewardsAmount"; type: "u64" }];
+      };
+    }
+  ];
+  errors: [
+    {
+      code: 300;
+      name: "InvalidTokenAccountAuthority";
+      msg: "Invalid token accountn authority";
+    },
+    {
+      code: 301;
+      name: "ArithmeticError";
+      msg: "Overflow or other checked arithmetic error";
+    }
+  ];
+  metadata: { address: "Sp1it1Djn2NmQvXLPnGM4zAXArxuchvSdytNt5n76Hm" };
+};
 
-
-
-  
-
-export type TokenAccountSplitV0 = IdlAccounts<SplTokenAccountSplitIDL>["tokenAccountSplitV0"]
-  
-          
+export type TokenAccountSplitV0 =
+  IdlAccounts<SplTokenAccountSplitIDL>["tokenAccountSplitV0"];
