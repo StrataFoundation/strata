c0f8b3403c6213d4f94fb6ad5647caa4c0481a14
Bryan <bryanzettler@gmail.com>
2021-11-10 17:35:30 -0600

    useBuy and useSell hooks plus fee hooks (#14)
    * useBuy and useSell hooks plus fee hooks
    
    * nameing change
    
    * update
    
    * bump version
    
    * bump spl-utils version

--- packages/react/src/contexts/solPrice.tsx
+++ packages/react/src/contexts/solPrice.tsx
@@ -1,16 +1,18 @@
+import React, { FC, ReactNode, createContext } from "react";
 import { useMarketPrice } from "../hooks";
-import React from "react";
 import { PublicKey } from "@solana/web3.js";
 
-export const SolPriceContext = React.createContext<number | undefined>(
-  undefined
-);
+export interface ISolPriceProviderProps {
+  children: ReactNode;
+}
+
+export const SolPriceContext = createContext<number | undefined>(undefined);
 
 const SOL_TO_USD_MARKET = new PublicKey(
   "9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT"
 );
 
-export const SolPriceProvider: React.FC = ({ children }) => {
+export const SolPriceProvider: FC<ISolPriceProviderProps> = ({ children }) => {
   const price = useMarketPrice(SOL_TO_USD_MARKET);
   return (
     <SolPriceContext.Provider value={price}>
