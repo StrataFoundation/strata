9f243196345f6e45db0f5b073020bbe5caf94d5b
Chewing Glass <chewinglass123@gmail.com>
2021-11-12 14:48:31 -0600

    Change staking params

--- tests/spl-token-staking.ts
+++ tests/spl-token-staking.ts
@@ -43,7 +43,7 @@ describe('spl-token-staking', () => {
 
   it('Initializes a stake instance and a stake', async () => {
     const tokenStakingAccount = await tokenStakingProgram.account.tokenStakingV0.fetch(tokenStaking)
-    expect(tokenStakingAccount.period).to.equal(5)
+    expect(tokenStakingAccount.period).to.equal(8)
     expect(tokenStakingAccount.rewardPercentPerPeriodPerLockupPeriod).to.equal(4294967295)
   });
 
