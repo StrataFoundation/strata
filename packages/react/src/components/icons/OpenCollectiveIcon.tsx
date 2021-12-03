b124447c36f9ae1be3593bc9c0a1d82382859bac
Bryan Zettler <bryanzettler@gmail.com>
2021-12-03 16:11:13 -0500

    add OpenCollectiveIcon

---
+++ packages/react/src/components/icons/OpenCollectiveIcon.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createIcon } from "@chakra-ui/react";
+
+export const OpenCollectiveIcon = createIcon({
+  displayName: "OpenCollective",
+  viewBox: "0 0 157 156",
+  path: [
+    <ellipse
+      cx="78.4026"
+      cy="77.9999"
+      fill="url(#a)"
+      rx="78.4026"
+      ry="77.9999"
+    />,
+    <path
+      fill="#fff"
+      d="M115.51 106.911c6.423-8.1015 10.259-18.347 10.259-29.4884 0-11.0783-3.793-21.2707-10.15-29.3499L93.0298 70.6614c1.1118 2.1792 1.7388 4.647 1.7388 7.2612 0 3.274-.9834 6.3183-2.6709 8.8537L115.51 106.911Z"
+    />,
+    <path
+      fill="#fff"
+      d="M108.494 114.067c-8.212 6.781-18.7431 10.856-30.2254 10.856-26.2336 0-47.5-21.267-47.5-47.5004 0-26.2335 21.2664-47.5 47.5-47.5 11.5529 0 22.1424 4.1245 30.3764 10.9812L85.9354 63.6136c-2.1559-1.0819-4.5903-1.691-7.1668-1.691-8.8366 0-16 7.1635-16 16 0 8.8366 7.1634 16 16 16 1.8462 0 3.6195-.3127 5.2696-.8881l24.4558 21.0325Z"
+    />,
+    <defs>
+      <radialGradient
+        id="a"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientTransform="rotate(71.764 -10.8869 60.373) scale(109.688 110.248)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset=".1054" stop-color="#8438FF" />
+        <stop offset=".2742" stop-color="#FC67BA" />
+        <stop offset=".4379" stop-color="#FF7B7B" />
+        <stop offset=".6757" stop-color="#FF3EBD" />
+        <stop offset=".9703" stop-color="#3561FF" />
+      </radialGradient>
+    </defs>,
+  ],
+});
--- packages/react/src/components/icons/index.ts
+++ packages/react/src/components/icons/index.ts
@@ -1,1 +1,2 @@
 export * from "./SolanaIcon";
+export * from "./OpenCollectiveIcon";
