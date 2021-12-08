8289d67f8967ac1388768578b5a3bc7543f8cf06
Chewing Glass <chewinglass123@gmail.com>
2021-12-07 18:46:06 -0600

    Fix twitter related hooks

--- packages/react/src/hooks/useOwnedAmountOfTwitterHandle.tsx
+++ packages/react/src/hooks/useOwnedAmountOfTwitterHandle.tsx
@@ -1,12 +1,13 @@
 import { PublicKey } from "@solana/web3.js";
+import { WUMBO_TWITTER_TLD } from "../hooks";
 import { useUserOwnedAmount } from "./bondingPricing";
 import { useTwitterTokenRef } from "./tokenRef";
 
 export function useOwnedAmountForOwnerAndHandle(
   owner: PublicKey | undefined | null,
   handle: string | undefined | null,
-  collective: PublicKey | undefined | null,
-  tld?: PublicKey
+  collective?: PublicKey | undefined | null,
+  tld: PublicKey = WUMBO_TWITTER_TLD
 ): { amount: number | undefined; loading: boolean } {
   const { info: tokenRef, loading: loadingRef } = useTwitterTokenRef(
     handle,
