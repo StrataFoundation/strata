3490eee8c126c9e07ecf42ca7f9e3c93bc8fa09b
Chewing Glass <chewinglass123@gmail.com>
2022-02-16 22:09:37 -0600

    Fix build

--- packages/docs/src/plugins/webpack-loader/index.js
+++ packages/docs/src/plugins/webpack-loader/index.js
@@ -4,37 +4,49 @@ const path = require("path")
 module.exports = function(context, options) {
   return {
     name: "custom-webpack-plugin",
+    
     configureWebpack(config, isServer, utils) {
       return {
         devtool: "source-map",
+        node: {
+          __dirname: true,
+        },
         resolve: {
           fallback: {
-            "fs": false,
-            "os": require.resolve("os-browserify/browser"),
-            "path": require.resolve("path-browserify"),
-            "crypto": require.resolve("crypto-browserify"),
-            "stream": require.resolve("stream-browserify") 
+            fs: false,
+            os: require.resolve("os-browserify/browser"),
+            path: require.resolve("path-browserify"),
+            crypto: require.resolve("crypto-browserify"),
+            stream: require.resolve("stream-browserify"),
           },
           alias: {
-            "@chakra-ui/react": path.resolve("../../node_modules/@chakra-ui/react"),
-            "@solana/wallet-adapter-react": path.resolve("../../node_modules/@solana/wallet-adapter-react"),
-            "@toruslabs/solana-embed": path.resolve("../../node_modules/@toruslabs/solana-embed"),
-          }
+            "@chakra-ui/react": path.resolve(
+              "../../node_modules/@chakra-ui/react"
+            ),
+            "@solana/wallet-adapter-react": path.resolve(
+              "../../node_modules/@solana/wallet-adapter-react"
+            ),
+            "@toruslabs/solana-embed": path.resolve(
+              "../../node_modules/@toruslabs/solana-embed"
+            ),
+          },
         },
         module: {
-          rules: [{
-            test: /.js$/,
-            type: "javascript/auto",
-            resolve: {
-              fullySpecified: false
-            }
-          }]
+          rules: [
+            {
+              test: /.js$/,
+              type: "javascript/auto",
+              resolve: {
+                fullySpecified: false,
+              },
+            },
+          ],
         },
         plugins: [
           new webpack.ProvidePlugin({
-            process: 'process/browser',
+            process: "process/browser",
           }),
-        ]
+        ],
       };
     }
   };
