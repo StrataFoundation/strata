6681ecdb729edd62478d115b70c4da07afcb3211
Chewing Glass <chewinglass123@gmail.com>
2022-02-18 11:05:36 -0600

    Add swap page

---
+++ packages/marketplace-ui/pages/swap/[mintKey]/index.tsx
@@ -0,0 +1,66 @@
+import { MetadataMeta } from "@/components/MetadataMeta";
+import { Box, Center, Container, Heading, Spinner } from "@chakra-ui/react";
+import {
+  Swap,
+  usePublicKey,
+  useTokenBondingFromMint
+} from "@strata-foundation/react";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage
+} from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React from "react";
+import { mintMetadataServerSideProps } from "@/utils/tokenMetadataServerProps";
+
+export const getServerSideProps: GetServerSideProps =
+  mintMetadataServerSideProps;
+
+export const SwapDisplay: NextPage = ({
+  name,
+  image,
+  description,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const { mintKey: mintKeyRaw } = router.query;
+  const mintKey = usePublicKey(mintKeyRaw as string);
+  const { info: tokenBonding, loading } = useTokenBondingFromMint(mintKey);
+
+  return (
+    <Box
+      w="full"
+      backgroundColor="#f9f9f9"
+      height="100vh"
+      overflow="auto"
+      paddingBottom="200px"
+    >
+      <Head>
+        <meta
+          property="twitter:url"
+          content={`https://marketplace.strataprotocol.com/bounty/${mintKey}/`}
+        />
+        <MetadataMeta name={name} description={description} image={image} />
+      </Head>
+      <Box padding="54px" backgroundColor="black.500" />
+      <Container mt="-72px" justify="stretch" maxW="460px">
+        <Heading mb={2} color="white" fontSize="24px" fontWeight={600}>
+          Swap
+        </Heading>
+        <Box zIndex={1} bg="white" shadow="xl" rounded="lg" minH="400px">
+          {loading && (
+            <Center>
+              <Spinner />
+            </Center>
+          )}
+          {!loading && tokenBonding && (
+            <Swap tokenBondingKey={tokenBonding!.publicKey} />
+          )}
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default SwapDisplay;
