de3bbf03498c012378cf3e38c9bbd58eb7915476
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2022-03-09 16:18:16 -0600

    Add support for more top level currencies (#144)

--- packages/react/src/hooks/useCoinGeckoPrice.ts
+++ packages/react/src/hooks/useCoinGeckoPrice.ts
@@ -2,15 +2,29 @@ import axios from "axios";
 import { useState } from "react";
 import { useInterval } from "./useInterval";
 
-export const useCoinGeckoPrice = (): number | undefined => {
+export async function getCoinGeckoPriceUsd(
+  tokenName: string = "solana"
+): Promise<number | undefined> {
+  let searchName = tokenName.toLowerCase();
+  // Some mappings
+  if (searchName === "grape") {
+    searchName = "grape-2"
+  }
+
+  const resp = await axios(
+    `https://api.coingecko.com/api/v3/simple/price?ids=${searchName}&vs_currencies=usd`
+  );
+  const result = resp.data[searchName];
+  if (result) {
+    return result.usd
+  }
+}
+
+export const useCoinGeckoPrice = (tokenName: string = "solana"): number | undefined => {
   const [price, setPrice] = useState<number>();
 
   useInterval(() => {
-    axios(
-      "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd"
-    )
-      .then((response) => setPrice(response.data.solana.usd))
-      .catch((e) => console.log(e));
+    getCoinGeckoPriceUsd(tokenName)?.then(p => setPrice(p)).catch(console.log);
   }, 2 * 60 * 1000);
 
   return price;
