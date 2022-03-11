287ac7a04ed0355a35ae9bdfc09035ec81fecbc7
Bryan Zettler <bryanzettler@gmail.com>
2022-03-11 16:37:31 -0600

    Back to webpack 5

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-  webpack5: false,
+  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
