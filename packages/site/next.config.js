386f6c8be5f831a898802cc8262d86730c55a2ab
Bryan Zettler <bryanzettler@gmail.com>
2022-03-11 16:32:57 -0600

    webpack 4?

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-  webpack5: true,
+  webpack5: false,
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
