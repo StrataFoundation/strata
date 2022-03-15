c0d65c0f4c47f4ae4b64298fd0d2c339fcbe72e2
Chewing Glass <chewinglass123@gmail.com>
2022-03-15 13:49:45 -0500

    Use enum

--- packages/marketplace-ui/src/utils/routes.ts
+++ packages/marketplace-ui/src/utils/routes.ts
@@ -4,36 +4,39 @@ export interface IRoute {
   module: Module;
 }
 
-type Module = "launchpad" | "marketplace";
+enum Module {
+  Marketplace = "marketplace",
+  Launchpad = "launchpad",
+};
 
 export const routes: Record<string, IRoute> = {
-  bounties: { path: "/bounties", params: [], module: "marketplace" },
-  newBounty: { path: "/bounties/new", params: [], module: "launchpad" },
+  bounties: { path: "/bounties", params: [], module: Module.Marketplace },
+  newBounty: { path: "/bounties/new", params: [], module: Module.Launchpad },
   bounty: {
     path: "/bounties/:mintKey",
     params: ["mintKey"],
-    module: "marketplace",
+    module: Module.Marketplace,
   },
   editBounty: {
     path: "/bounties/:mintKey/edit",
     params: ["mintKey"],
-    module: "launchpad",
+    module: Module.Launchpad,
   },
-  sales: { path: "/sales", params: [], module: "marketplace" },
-  newSale: { path: "/sales/new", params: [], module: "launchpad" },
-  sale: { path: "/sales/:mintKey", params: ["mintKey"], module: "marketplace" },
-  newLbc: { path: "/lbcs/new", params: [], module: "launchpad" },
+  sales: { path: "/sales", params: [], module: Module.Marketplace },
+  newSale: { path: "/sales/new", params: [], module: Module.Launchpad },
+  sale: { path: "/sales/:mintKey", params: ["mintKey"], module: Module.Marketplace },
+  newLbc: { path: "/lbcs/new", params: [], module: Module.Launchpad },
   tokenLbc: {
     path: "/lbcs/token-offering/:mintKey",
     params: ["mintKey"],
-    module: "marketplace",
+    module: Module.Marketplace,
   },
   tokenOffering: {
     path: "/token-offering/:mintKey",
     params: ["mintKey"],
-    module: "marketplace",
+    module: Module.Marketplace,
   },
-  swap: { path: "/swap/:mintKey", params: ["mintKey"], module: "marketplace" },
+  swap: { path: "/swap/:mintKey", params: ["mintKey"], module: Module.Marketplace },
 };
 
 const MARKETPLACE_URI = process.env.NEXT_PUBLIC_MARKETPLACE_URI || "";
@@ -44,7 +47,7 @@ export function route(route: IRoute, params: Record<string, string | undefined>)
     return acc;
   }, route.path);
 
-  const prefix = route.module === "marketplace" ? MARKETPLACE_URI : LAUNCHPAD_URI;
+  const prefix = route.module === Module.Marketplace ? MARKETPLACE_URI : LAUNCHPAD_URI;
 
   return prefix + relativeRoute;
 }
