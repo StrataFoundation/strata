f7c0dac58f8cc725c03662018229b87ee311e382
Bryan <bryanzettler@gmail.com>
2022-03-22 13:48:59 -0500

    Site analytics (#166)
    * Site analytics
    
    * Move default seo

--- packages/site/pages/_app.tsx
+++ packages/site/pages/_app.tsx
@@ -1,29 +1,47 @@
-import SEO from "../next-seo.config";
 import "../styles/globals.css";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import "@/utils/bufferFill";
-import { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "@/utils/gtag";
 import { Toaster } from "react-hot-toast";
 
+import SEO from "../next-seo.config";
 import { Providers } from "@/components/Providers";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { IS_PRODUCTION } from "@/constants";
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <Providers>
-    <DefaultSeo {...SEO} />
-    <Header />
-    <Component {...pageProps} />
-    <Toaster
-      position="bottom-center"
-      containerStyle={{
-        margin: "auto",
-        width: "420px",
-      }}
-    />
-    <Footer />
-  </Providers>
-);
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (IS_PRODUCTION) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <Providers>
+      <DefaultSeo {...SEO} />
+      <Header />
+      <Component {...pageProps} />
+      <Toaster
+        position="bottom-center"
+        containerStyle={{
+          margin: "auto",
+          width: "420px",
+        }}
+      />
+      <Footer />
+    </Providers>
+  );
+};
 
 export default App;
