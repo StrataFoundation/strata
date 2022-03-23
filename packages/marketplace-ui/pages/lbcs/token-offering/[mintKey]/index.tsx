0e018ecfa31ac543cda034d9a748348468fac44e
Bryan <bryanzettler@gmail.com>
2022-03-23 16:57:15 -0500

    Update packages/marketplace-ui/pages/lbcs/token-offering/[mintKey]/index.tsx

--- packages/marketplace-ui/pages/lbcs/token-offering/[mintKey]/index.tsx
+++ packages/marketplace-ui/pages/lbcs/token-offering/[mintKey]/index.tsx
@@ -60,7 +60,7 @@ export const LbcDisplay: NextPage = ({
       paddingBottom="200px"
     >
       <MetadataMeta
-        title={`Starta LBC Token Offering | ${name}`}
+        title={`Strata LBC Token Offering | ${name}`}
         description={description}
         image={image}
         url={`${SITE_URL}/bounty/${mintKey}/`}
