d8b58cfe232960e5e212d1716e439c59a9b25674
Bryan Zettler <bryanzettler@gmail.com>
2022-04-05 21:39:36 -0400

    move metaAttributes to top

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -21,15 +21,15 @@ const config = {
   <html <%~ it.htmlAttributes %>>
     <head>
       <meta charset="UTF-8">
+      <%~ it.headTags %>
+      <% it.metaAttributes.forEach((metaAttribute) => { %>
+        <%~ metaAttribute %>
+      <% }); %>      
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="generator" content="Docusaurus v<%= it.version %>">
       <% if (it.noIndex) { %>
         <meta name="robots" content="noindex, nofollow" />
       <% } %>
-      <%~ it.headTags %>
-      <% it.metaAttributes.forEach((metaAttribute) => { %>
-        <%~ metaAttribute %>
-      <% }); %>
       <% it.stylesheets.forEach((stylesheet) => { %>
         <link rel="stylesheet" href="<%= it.baseUrl %><%= stylesheet %>" />
       <% }); %>
