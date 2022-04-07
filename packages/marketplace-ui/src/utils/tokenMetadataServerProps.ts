2206db6d9e128ef356eecf2fa9e3d9c829fbc716
Chewing Glass <chewinglass123@gmail.com>
2022-04-07 12:36:49 -0500

    Fix auth endpoint

--- packages/marketplace-ui/src/utils/tokenMetadataServerProps.ts
+++ packages/marketplace-ui/src/utils/tokenMetadataServerProps.ts
@@ -6,13 +6,43 @@ import { DEFAULT_ENDPOINT } from "../components/Wallet";
 import { SplTokenMetadata } from "@strata-foundation/spl-utils";
 import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 import { getClusterAndEndpoint } from "../hooks";
+import { Base64 } from "js-base64";
+import axios from "axios";
+import { tokenAuthFetchMiddleware } from "@strata-foundation/web3-token-auth";
+
+async function getToken(): Promise<string> {
+  if (process.env.ISSUER) {
+    const token = Base64.encode(
+      `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+    );
+    const { access_token } = (
+      await axios.post(
+        `${process.env.ISSUER}/token`,
+        "grant_type=client_credentials",
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${token}`,
+          },
+        }
+      )
+    ).data;
+    return access_token;
+  }
+
+  return "";
+}
 
 export const mintMetadataServerSideProps: GetServerSideProps = async (
   context
 ) => {
   const { endpoint } = getClusterAndEndpoint((context.query.cluster || DEFAULT_ENDPOINT) as string);
 
-  const connection = new Connection(endpoint);
+  const connection = new Connection(endpoint, {
+    fetchMiddleware: tokenAuthFetchMiddleware({
+      getToken
+    })
+  });
   const provider = new Provider(
     connection,
     new NodeWallet(Keypair.generate()),
