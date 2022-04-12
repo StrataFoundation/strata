12b644d6f072cb40c43550f0bd7225b1911fdf99
Chewing Glass <chewinglass123@gmail.com>
2022-04-12 14:38:13 -0500

    Fix cors

--- packages/marketplace-ui/pages/api/get-token.ts
+++ packages/marketplace-ui/pages/api/get-token.ts
@@ -2,6 +2,27 @@ import { Base64 } from "js-base64";
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
+import Cors from "cors";
+import initMiddleware from "../../src/utils/initMiddleware";
+
+const domain =
+  process.env.NODE_ENV === "development"
+    ? "localhost:.*$"
+    : "strataprotocol.com$";
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+
+  Cors({
+    // Only allow requests with GET
+    methods: ["GET"],
+    origin: [
+      new RegExp(`\.${domain}`),
+      new RegExp(domain),
+    ],
+  })
+);
+
 type Data = {
   access_token: string;
 };
@@ -10,6 +31,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await cors(req, res);
+
   const token = Base64.encode(
     `${process.env.NEXT_PUBLIC_CLIENT_ID}:${process.env.NEXT_PUBLIC_CLIENT_SECRET}`
   );
