dcc4de9ba75b0d3e86407aac3a50cbea5368cfff
Chewing Glass <chewinglass123@gmail.com>
2022-04-12 14:18:47 -0500

    Attempt 2 at env vars

--- packages/docs/src/plugins/webpack-loader/index.js
+++ packages/docs/src/plugins/webpack-loader/index.js
@@ -45,9 +45,9 @@ module.exports = function (context, options) {
         },
         plugins: [
           new Dotenv(),
-          new webpack.DefinePlugin({ 
-            "process.env.GET_TOKEN_ENDPOINT": process.env.GET_TOKEN_ENDPOINT 
-          }),
+          new webpack.EnvironmentPlugin([
+            "GET_TOKEN_ENDPOINT",
+          ]),
           new webpack.ProvidePlugin({
             process: "process/browser",
           }),
