f35723f11fa2a1400d9ec6903e7a48346cf68e1b
Chewing Glass <chewinglass123@gmail.com>
2022-04-16 10:52:48 -0500

    chore(release): publish 3.5.2

--- packages/react/src/hooks/useSwap.ts
+++ packages/react/src/hooks/useSwap.ts
@@ -10,7 +10,7 @@ export type SwapArgs = {
   extraInstructions?: (args: {
     tokenBonding: ITokenBonding;
     isBuy: boolean;
-    amount: BN;
+    amount: BN | undefined;
   }) => Promise<InstructionResult<null>>;
 };
 
