862e5c1bec423fbe7867b00b619722f1f40f02be
Bryan Zettler <bryanzettler@gmail.com>
2022-06-09 12:22:54 -0500

    Alt change

--- packages/site/components/Header.tsx
+++ packages/site/components/Header.tsx
@@ -84,7 +84,7 @@ export const Header: React.FC = () => {
             >
               <Image
                 src="/riptideAppBarBanner.png"
-                alt="riptide hackathon 1st place winner"
+                alt="riptide hackathon 1st place winner DAOs Track"
                 height="40px"
               />
             </Link>
