2f47c8c126a2fc987e8546eb03312b655e7a0973
ThornM9 <tm.jalal@gmail.com>
2022-07-08 14:10:28 +1000

    ignore

--- packages/chat-ui/src/utils/gtag.ts
+++ packages/chat-ui/src/utils/gtag.ts
@@ -3,6 +3,7 @@ import { GA_TRACKING_ID, IS_PRODUCTION } from "../constants";
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
   if (GA_TRACKING_ID && IS_PRODUCTION) {
+    //@ts-ignore
     window.gtag("config", GA_TRACKING_ID, {
       page_path: url,
     });
@@ -19,6 +20,7 @@ type GTagEvent = {
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
   if (GA_TRACKING_ID && IS_PRODUCTION) {
+    //@ts-ignore
     window.gtag("event", action, {
       event_category: category,
       event_label: label,
