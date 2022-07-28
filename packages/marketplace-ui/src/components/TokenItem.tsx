8d46204335e270a8456ca4f40d93c8641f7b2d25
ThornM9 <tm.jalal@gmail.com>
2022-07-28 14:39:54 +1000

    add spacing

--- packages/marketplace-ui/src/components/TokenItem.tsx
+++ packages/marketplace-ui/src/components/TokenItem.tsx
@@ -30,60 +30,60 @@ export const TokenItem = ({mint, updateRef, isIntermediateToken}: {
   return (
     <>
       { hasAnyAuth && shouldDisplay ? (
-          <Flex bgColor="white" borderRadius="8px" w="full" h="7em" alignItems="center">
-            <Flex
-              onClick={() => {
-                router.push(
-                  route(routes.tokenAdmin, {
-                    mintKey: mint.toString(),
-                  }),
-                  undefined,
-                  { shallow: true }
-                )
-              }}
-              cursor="pointer"
-              alignItems="center"
-            >
-              <Image
-                alt="Token logo"
-                marginLeft="2em"
-                w="50px"
-                h="50px"
-                borderRadius="50%"
-                src={image}
-              />
+        <Flex bgColor="white" borderRadius="8px" w="full" h="7em" alignItems="center" mb="10px !important">
+          <Flex
+            onClick={() => {
+              router.push(
+                route(routes.tokenAdmin, {
+                  mintKey: mint.toString(),
+                }),
+                undefined,
+                { shallow: true }
+              )
+            }}
+            cursor="pointer"
+            alignItems="center"
+          >
+            <Image
+              alt="Token logo"
+              marginLeft="2em"
+              w="50px"
+              h="50px"
+              borderRadius="50%"
+              src={image}
+            />
 
-                <Text 
-                  fontSize="xl" 
-                  textAlign="left" 
-                  fontWeight="bold"
-                  paddingLeft="10px"
-                >
-                  {data?.name}
-                </Text>
-            </Flex>
-            <Flex 
-              marginLeft="auto"
-              onClick={() => {
-                router.push(
-                  route(routes.tokenAdmin, {
-                    mintKey: mint.toString(),
-                  }),
-                  undefined,
-                  { shallow: true }
-                )
-              }} 
-              cursor="pointer"
-            >
-              <Text marginRight="10px">Manage Token</Text>
-              <Icon 
-                w="24px" h="24px" 
-                as={MdSettings} 
-                
-                marginRight="2em" 
-              />
-            </Flex>
+              <Text 
+                fontSize="xl" 
+                textAlign="left" 
+                fontWeight="bold"
+                paddingLeft="10px"
+              >
+                {data?.name}
+              </Text>
           </Flex>
+          <Flex 
+            marginLeft="auto"
+            onClick={() => {
+              router.push(
+                route(routes.tokenAdmin, {
+                  mintKey: mint.toString(),
+                }),
+                undefined,
+                { shallow: true }
+              )
+            }} 
+            cursor="pointer"
+          >
+            <Text marginRight="10px">Manage Token</Text>
+            <Icon 
+              w="24px" h="24px" 
+              as={MdSettings} 
+              
+              marginRight="2em" 
+            />
+          </Flex>
+        </Flex>
       ) : null}
     </>
   )
