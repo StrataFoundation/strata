2a37b74f47a2a5a1dd62cf7c17c8c01cc0f8285d
Chewing Glass <chewinglass123@gmail.com>
2022-07-29 14:28:15 -0500

    Vite fucking sucks

---
+++ packages/accelerator/vite.config.ts
@@ -0,0 +1,29 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "SplTokenCollective",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "crypto"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "crypto": "Crypto"
+        },
+      },
+    },
+  },
+});
---
+++ packages/chat/vite.config.ts
@@ -0,0 +1,46 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "SplTokenCollective",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "crypto",
+        "@project-serum/anchor",
+        "@project-serum/borsh",
+        "@solana/web3.js",
+        "assert",
+        "base64-js",
+        "bn.js",
+        "bs58",
+        "buffer",
+        "camelcase",
+        "eventemitter3",
+        "js-sha256",
+        "pako",
+        "toml",
+        "fs"
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
