2830feb61dc01dd13fad86acbd28658a29bcb7c0
Chewing Glass <chewinglass123@gmail.com>
2022-09-12 12:24:41 -0500

    Add back mainnet

--- packages/docs/src/components/CloseOutWumbo/index.tsx
+++ packages/docs/src/components/CloseOutWumbo/index.tsx
@@ -25,27 +25,28 @@ import { ITokenWithMetaAndAccount } from "@strata-foundation/spl-token-collectiv
 import { truthy } from "@strata-foundation/spl-utils";
 import { closeOutWumboSubmit } from "./closeOutWumboSubmit";
 import styles from "./styles.module.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 const OPEN_COLLECTIVE = "3cYa5WvT2bgXSLxxu9XDJSHV3x5JZGM91Nc3B7jYhBL7";
 
 const MainnetGuard = ({ children = null as any }) => {
   const { endpoint, setClusterOrEndpoint } = useEndpoint();
 
-  // if (endpoint.includes("devnet")) {
-  //   return (
-  //     <div className={styles.container}>
-  //       <h3>Recoup SOL from Wumbo</h3>
-  //       <button
-  //         onClick={() => {
-  //           setClusterOrEndpoint(WalletAdapterNetwork.Mainnet);
-  //         }}
-  //         className="white button button--primary"
-  //       >
-  //         Switch to Mainnet
-  //       </button>
-  //     </div>
-  //   );
-  // }
+  if (endpoint.includes("devnet")) {
+    return (
+      <div className={styles.container}>
+        <h3>Recoup SOL from Wumbo</h3>
+        <button
+          onClick={() => {
+            setClusterOrEndpoint(WalletAdapterNetwork.Mainnet);
+          }}
+          className="white button button--primary"
+        >
+          Switch to Mainnet
+        </button>
+      </div>
+    );
+  }
 
   return children;
 };
